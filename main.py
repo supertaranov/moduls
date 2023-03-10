a6d3acd1ecba6d29e666c8f10c4b8cba5bfe63d4
Leonid Taranov <supertaranov@gmail.com>
2023-03-10 23:13:10 +0700

    update import

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from salary import calculate_salary
-from people import get_employees
+from application.salary import calculate_salary
+from application.db.people import get_employees
 import datetime
 import pytz
 
